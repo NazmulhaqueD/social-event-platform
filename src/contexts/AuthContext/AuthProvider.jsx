098b74aba130090a25008f03b05d1769d21ec139
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-06-12 01:51:08 +0600

    register page register functionality with firebase auth done

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,16 +1,26 @@
 import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { app } from './firebaseAuth/firebase.config';
 
-
-const AuthContext = createContext();
+const auth = getAuth(app)
+export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    const signUp = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
     const userInfo = {
+        signUp,
         user,
-        setUser
+        setUser,
+        loading,
+        setLoading
     }
     return (
         <AuthContext value={userInfo}>
--- src/page/Register/Register.jsx
+++ src/page/Register/Register.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router';
+import { AuthContext } from '../../contexts/AuthContext/AuthProvider';
 
 const Register = () => {
+
+    const { signUp } = useContext(AuthContext);
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const registerData = Object.fromEntries(formData.entries());
+        const { email, password } = registerData;
+
+        signUp(email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
+
     return (
 
         <div className="card bg-base-100 w-full mx-auto my-16 max-w-sm shrink-0 shadow-2xl">
             <h1 className="text-5xl font-bold text-center mt-6">Register now!</h1>
             <div className="card-body">
-                <form className='space-y-4'>
+                <form onSubmit={handleSignUp} className='space-y-4'>
                     <label className="label">Name</label>
                     <input type="text" name='name' className="input" placeholder="Your Name" />
 
@@ -18,7 +38,7 @@ const Register = () => {
                     <label className="label">Password</label>
                     <input type="password" name='password' className="input" placeholder="Password" />
                     <div><a className="link link-hover">Forgot password?</a></div>
-                    <button className="btn btn-neutral w-full mt-4">Register</button>
+                    <button type='submit' className="btn btn-neutral w-full mt-4">Register</button>
                     <p>Already have an account? <NavLink className={'text-teal-400 underline font-semibold text-xl'}>Login</NavLink></p>
                 </form>
             </div>
